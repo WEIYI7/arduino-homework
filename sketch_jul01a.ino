26a84e81ec522d2e484ae68f84bc9f1ad421685a
WEIYI7 <2712997934@qq.com>
2017-07-02 07:57:58 +0800

    Update sketch_jul01a.ino

--- sketch_jul01a.ino
+++ sketch_jul01a.ino
@@ -6,7 +6,7 @@ void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
 
-
+pinMode(13, OUTPUT);
 pinMode(TrigPin, OUTPUT);
 pinMode(EchoPin, INPUT);
 }
@@ -23,9 +23,11 @@ digitalWrite(TrigPin, LOW);
   cm=(int(cm*100))/100;
    if (cm <= 10){
    r=0;
+     digitalWrite(13,HIGH);
    }
     else{
     r=1;
+      digitalWrite(13,LOW);
     }
    Serial.print(r);      
     
