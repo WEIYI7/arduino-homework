ee7e152b9c9ebc5ca614cb940d222ecfb64c19ba
WEIYI7 <2712997934@qq.com>
2017-07-02 08:14:59 +0800

    Arduino代码
    开源与创意建模大作业Arduino代码

---
+++ sketch_jul01a.ino
@@ -0,0 +1,34 @@
+const int EchoPin = 3;
+const int TrigPin = 2;
+int r;   
+float cm;
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+
+pinMode(13, OUTPUT);
+pinMode(TrigPin, OUTPUT);
+pinMode(EchoPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(TrigPin, LOW); 
+delayMicroseconds(2);
+digitalWrite(TrigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(TrigPin, LOW); 
+  
+ cm = pulseIn(EchoPin, HIGH) / 58.0;
+  cm=(int(cm*100))/100;
+   if (cm <= 10){
+   r=0;
+   digitalWrite(13,HIGH);
+   }
+    else{
+    r=1;
+    digitalWrite(13,LOW);
+    }
+   Serial.print(r);      
+    
+}
